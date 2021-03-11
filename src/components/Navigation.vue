dfa902936fe238a7482da963ec5b2485711d2380
Thomas Artmann <thwohmas@gmail.com>
2021-03-11 20:50:31 +0100

    #

---
+++ src/components/Navigation.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand href="/">
+      <img src="../assets/boatware3.png" class="d-inline-block align-top" alt="Kitten" width="32" height="32">
+      boatware
+    </b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="/">Homes</b-nav-item>
+        <b-nav-item href="/about">Link</b-nav-item>
+        <b-nav-item href="/about" disabled>Disabled</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+}
+</script>
+
+<style scoped lang="scss">
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,39 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import { BootstrapVue, IconsPlugin, NavbarPlugin } from 'bootstrap-vue'
+import './assets/scss/main.scss';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.use(NavbarPlugin);
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
+
+/**
+ * Prevents the page to be fully reloaded when an internal link is clicked.
+ * Instead the router will be asked to handle the link like a <router-link>.
+ * @param event {Event}
+ */
+function pushClickedLinks(event) {
+  if (event.target.tagName === "A") {
+    event.preventDefault();
+    console.debug(event.target.getAttribute("href").match(/^\//));
+    let href = event.target.getAttribute("href");
+    if (href.match(/^\/[^/]/) !== undefined || (href.match(/^\//) !== undefined && href.length === 1)) {
+      event.preventDefault();
+      router.push({path: href, query: {lol: "lol"}});
+    }
+  }
+}
+
+if (document.addEventListener) {
+  document.addEventListener("click", pushClickedLinks, false);
+}
+
